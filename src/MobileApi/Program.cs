2c02a672b5bb4648f207b147615af431c5bb6a86
mohitt2d <104139311+mohitt2d@users.noreply.github.com>
2022-04-28 09:51:29 +0000

    Update Program.cs

--- src/MobileApi/Program.cs
+++ src/MobileApi/Program.cs
@@ -36,7 +36,7 @@ namespace UltimatePlaylist.MobileApi
 
             try
             {
-                Log.Information("Starting web host. 123");
+                Log.Information("Starting web host. 12345");
 
                 var host = CreateHostBuilder(args).Build();
 
@@ -48,7 +48,7 @@ namespace UltimatePlaylist.MobileApi
             }
             catch (Exception ex)
             {
-                Log.Fatal(ex, "Host terminated unexpectedly. 123");
+                Log.Fatal(ex, "Host terminated unexpectedly. 12345");
 
                 return 1;
             }
