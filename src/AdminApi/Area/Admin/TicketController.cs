88ba668b8fe11db36b4a153fece76fd0758ad25c
mohitt2d <104139311+mohitt2d@users.noreply.github.com>
2022-10-16 16:58:56 +0000

    remove space letter from token

--- src/AdminApi/Area/Admin/TicketController.cs
+++ src/AdminApi/Area/Admin/TicketController.cs
@@ -46,7 +46,7 @@ namespace UltimatePlaylist.AdminApi.Area.Admin
         [ProducesEnvelope(typeof(UserTicketStatsRequestModel), StatusCodes.Status200OK)]
         public async Task<IActionResult> UserTicketStatus(string token)
         {
-            string x = IdentityService.GetUserId(token.Replace("%20", ""));
+            string x = IdentityService.GetUserId(token.Replace("%20", ""));// remove space letter from token
             long id = long.Parse(x, System.Globalization.NumberStyles.AllowThousands | System.Globalization.NumberStyles.AllowLeadingSign);
             return await TicketService.UserTicketStatus(id)
                 .Map(isErrorTriggered => new UserTicketStatsRequestModel() { IsErrorTriggered = isErrorTriggered })
